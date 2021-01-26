779ae54a1e077b112ce89d6a911d3c1ab2a79930
sumit kumar bighaniya <sb78639@gmail.com>
2021-01-26 08:53:35 +0530

    Firebase config removed

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyiz-z2x3iiYEgFoMlyTU1M0-fWmapV3E",
-  authDomain: "keepapp-49048.firebaseapp.com",
-  projectId: "keepapp-49048",
-  storageBucket: "keepapp-49048.appspot.com",
-  messagingSenderId: "769608612615",
-  appId: "1:769608612615:web:c59b5a1ea9062cc21ae3a2",
-  measurementId: "G-N50L4CQ0RH"
+  // Firebase App config comes here
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebaseApp.auth();
